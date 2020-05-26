072ecca85db6756dc55e8b12eac3279780612ffe
dbacall <dbacall@hotmail.co.uk>
2020-05-26 16:31:07 +0100

    refactor: extract set up from App.test.js to setupTests.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
+import { shallow } from "enzyme";
 
 let wrapper;
 
