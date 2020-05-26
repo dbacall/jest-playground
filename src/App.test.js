6e13924544c145d5a467d98c9be96687a582870b
dbacall <dbacall@hotmail.co.uk>
2020-05-26 15:14:02 +0100

    feat: add title for word changing app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe("word change component", () => {
+  test("shows heading", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toContain("Crazy Word Changer!!");
+  });
 });
