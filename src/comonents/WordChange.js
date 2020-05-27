173864ca196bb7b5095e218ca4adc1b2964f376f
dbacall <dbacall@hotmail.co.uk>
2020-05-27 16:36:47 +0100

    feat: add wordchange component

---
+++ src/comonents/WordChange.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function WordChange() {
+  return (
+    <div className="App">
+      <h1>Crazy Word Changer!!</h1>
+      <input
+        type="text"
+        placeholder="Your word..."
+        id="word-input"
+        // value={this.state.inputWord}
+        // onChange={this.handleChange}
+      />
+      {/* <button type="submit" id="btn" onClick={this.handleSubmit}>
+        Update
+      </button>
+
+      <div id="your-word">{this.state.updatedWord}</div> */}
+    </div>
+  );
+}
+
+export default WordChange;
---
+++ src/tests/WordChange.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import WordChange from "../comonents/WordChange";
+import { shallow } from "enzyme";
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<WordChange />);
+});
+
+describe("word change component with hooks", () => {
+  test("render shows heading", () => {
+    expect(wrapper.find("h1").text()).toContain("Crazy Word Changer!!");
+  });
+
+  test("renders input for word", () => {
+    expect(wrapper.find("#word-input").prop("placeholder")).toBe(
+      "Your word..."
+    );
+  });
+
+  // test("renders submit button", () => {
+  //   expect(wrapper.find("#btn").text()).toBe("Update");
+  // });
+
+  // test("renders state change on input change and updates div when click button", () => {
+  //   wrapper.find("#word-input").simulate("change", {
+  //     target: {
+  //       value: "word",
+  //     },
+  //   });
+  //   console.log(wrapper.state());
+  //   wrapper.find("#btn").simulate("click");
+  //   console.log(wrapper.state());
+
+  //   expect(wrapper.find("#your-word").text()).toBe("word");
+  // });
+});
